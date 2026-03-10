7a595f5bfb318f86746380417dd3e9c6d159053f
Kenny <auguste.kenny@monboreal.ca>
2026-03-09 23:38:28 -0400

    Commit #2 Creation des classes et methodes

---
+++ Gestion_de_voitures_de_service.py
@@ -0,0 +1,23 @@
+class Employe:
+    def __init__(self, numeroPermis, nom, prenom):
+        self.numeroPermis = numeroPermis
+        self.nom = nom
+        self.prenom = prenom
+        self.voitureService = None
+    def afficherInfos(self):
+        print(f"L'employé: {self.prenom, self.nom}")
+        print(f"Numéro de permis : {self.numeroPermis}")
+        if self.voitureService:
+            print("La voiture est attribuée: ")
+            self.voitureService.afficherInfos()
+        else:
+            print("Pas de voiture attribuée")
+    def affecterVoiture(self, voiture):
+        if self.voitureService:
+            print("L'employé possède déjà une voiture")
+            return
+        if self.voiturees:
+            print("La voiture est attribuée: ")
+            return
+        self.voitureService = voiture
+        voiture.voiturees = self
